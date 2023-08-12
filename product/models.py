034ba8064a42b7ae655a7f48261354c627e5506c
Ahmed Magdy <94301115+AhmedDR200@users.noreply.github.com>
2023-08-12 15:00:08 +0300

    PayPal Checkout Integration

--- product/models.py
+++ product/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Order(models.Model):
+    order_id = models.IntegerField()
+    order_completed = models.BooleanField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.order_id)
--- product/views.py
+++ product/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Product , Order
 # Create your views here.
+
+
+def product_detail(request, id):
+    product = Product.objects.get(id=id)
+    return render (request, 'product/detail.html', {'product':product})
